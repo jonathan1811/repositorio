6b6bd0ced7d285fd6892ef9db48d5f9a71dc5ad9
Jonathan Zurita <zuritapenajonathan@gmail.com>
2022-06-30 20:14:01 -0400

    primer

---
+++ src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author zurit
+ */
+public class Mavenproject2 {
+    
+    public static void main(String[] args) {
+      String palabra = "Jonathan";
+		System.out.println(palabra);
+		StringBuilder palabra2 = new StringBuilder(palabra);
+		palabra = palabra2.reverse().toString();
+		System.out.println(palabra);
+    
+  }
+}
+    
+
